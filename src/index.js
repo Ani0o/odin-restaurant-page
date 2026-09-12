af952c679bd82ad02825b1b2a25940c47b194a9d
Ani <205335742+Ani0o@users.noreply.github.com>
2026-09-13 02:55:24 +0530

    Add tab change functionality using event handlers

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@ import "./styles.css";
 import loadHome from "./home.js";
 import loadMenu from "./menu.js";
 
-loadMenu();
+const homeButton = document.querySelector(".home");
+const menuButton = document.querySelector(".menu");
+const aboutButton = document.querySelector(".about");
+
+homeButton.addEventListener("click", (e) => {
+    loadHome();
+});
+
+menuButton.addEventListener("click", (e) => {
+    loadMenu();
+});
+
+aboutButton.addEventListener("click", (e) => {
+    loadAbout();
+});
